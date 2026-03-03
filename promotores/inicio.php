ef6d8ffe28fbe539a6a3b1a5409231f93d96a070
VelaMaster <pdiegovela@gmail.com>
2026-03-03 13:21:55 -0600

    Menu promotor

--- promotores/inicio.php
+++ promotores/inicio.php
@@ -19,79 +19,36 @@ $nombre_usuario = isset($_SESSION['nombre']) ? $_SESSION['nombre'] : $_SESSION['
 
 <body>
     <nav class="navbar nav-base-moderna" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-        <a class="navbar-item logo-ajustado" href="#">
-            <span class="icon is-medium mr-2"><i class="fas fa-bolt fa-lg"></i></span>
-            <strong class="texto-logotipo">Leche para el bienestar.</strong>
-        </a>
+        <div class="navbar-brand">
+            <a class="navbar-item logo-ajustado" href="#">
+                <span class="icon is-medium mr-2"><i class="fas fa-bolt fa-lg"></i></span>
+                <strong class="texto-logotipo">Leche para el bienestar.</strong>
+            </a>
 
-        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navMenuLiconsa">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-        </a>
-    </div>
-
-    <div id="navMenuLiconsa" class="navbar-menu">
-        <div class="navbar-start">
-            
-<div class="navbar-item has-dropdown is-hoverable">
-    <a class="navbar-link is-arrowless nav-enlace">Inventarios</a>
-    
-    <div class="navbar-dropdown is-boxed glass-menu">
-        <a class="navbar-item">Stock</a>
-        <a class="navbar-item">Entradas</a>
-        <a class="navbar-item">Kardex</a>
-    </div>
-</div>
-
+            <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navMenuLiconsa">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
         </div>
-    </div>
-</nav>
-    <section class="section">
-        <div class="container">
-            <div class="columns is-centered">
-                <div class="column is-10">
 
-                    <div class="columns is-multiline">
-                        <div class="column is-4">
-                            <div class="box has-text-centered" style="transition: transform 0.3s;" onmouseover="this.style.transform='translateY(-5px)'" onmouseout="this.style.transform='translateY(0)'">
-                                <span class="icon is-large has-text-success">
-                                    <i class="fas fa-truck-loading fa-3x"></i>
-                                </span>
-                                <h3 class="title is-4 mt-4">Inventario mensual de leche en polvo.</h3>
-                                <p>Generar, revisar o actualizar inventariio.</p>
-                                <button class="button is-success is-rounded is-fullwidth mt-4">Entrar</button>
-                            </div>
-                        </div>
+        <div id="navMenuLiconsa" class="navbar-menu">
+            <div class="navbar-start">
 
-                        <div class="column is-4">
-                            <div class="box has-text-centered" style="transition: transform 0.3s;" onmouseover="this.style.transform='translateY(-5px)'" onmouseout="this.style.transform='translateY(0)'">
-                                <span class="icon is-large has-text-warning">
-                                    <i class="fas fa-file-invoice fa-3x"></i>
-                                </span>
-                                <h3 class="title is-4 mt-4">Reporte mensual de la operacion en lecherias con venta de leche en polvo.</h3>
-                                <p>Generar, revisar y descargar reportes mensuales.</p>
-                                <button class="button is-warning is-rounded is-fullwidth mt-4">Entrar</button>
-                            </div>
-                        </div>
-                        <div class="column is-4">
-                            <div class="box has-text-centered" style="transition: transform 0.3s;" onmouseover="this.style.transform='translateY(-5px)'" onmouseout="this.style.transform='translateY(0)'">
-                                <span class="icon is-large has-text-warning">
-                                    <i class="fas fa-file-invoice fa-3x"></i>
-                                </span>
-                                <h3 class="title is-4 mt-4">Requerimiento de leche.</h3>
-                                <p>Generar, revisar y descargar reportes mensuales.</p>
-                                <button class="button is-loading is-rounded is-fullwidth mt-4">Entrar</button>
-                            </div>
-                        </div>
+                <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link is-arrowless nav-enlace">Inventarios</a>
+
+                    <div class="navbar-dropdown is-boxed glass-menu">
+                        <a class="navbar-item">Stock</a>
+                        <a class="navbar-item">Entradas</a>
+                        <a class="navbar-item">Kardex</a>
                     </div>
-
                 </div>
+
             </div>
         </div>
-    </section>
+    </nav>
     <script src="../js/temas.js"> </script>
 </body>
 
